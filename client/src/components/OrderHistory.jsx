04203e727f111bbc43ecc478e7c36d9bf7f62c46
Tanish Jain <tanishjain020205@gmail.com>
2025-03-30 20:31:49 +0530

    Deployed

--- client/src/components/OrderHistory.jsx
+++ client/src/components/OrderHistory.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-const API_URL = `http://localhost:5000`;
+const API_URL = import.meta.env.VITE_API_URL; // API URL from environment variables
 
 const OrderHistory = () => {
   const [orders, setOrders] = useState([]);
