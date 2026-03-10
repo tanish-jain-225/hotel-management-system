fc2b3c82bf3c9b3e4074bbf8f41742c3e139ae0d
Tanish Jain <tanishjain020205@gmail.com>
2026-03-10 15:15:40 +0530

    Deployed

--- client/src/services/helper.js
+++ client/src/services/helper.js
@@ -2,7 +2,7 @@ let API_URL;
 
 // Dynamically set API_URL based on environment weather dev or prod
 
-if (import.meta.env == "production") {
+if (import.meta.env.MODE === "production") {
     API_URL = import.meta.env.VITE_API_URL_PROD;
 } else {
     API_URL = import.meta.env.VITE_API_URL_DEV;
